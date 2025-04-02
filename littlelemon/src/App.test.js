0e3f881a846e011cf63397839e67f6c3c3c64740
Blackhole <128896418+SerhiiSkyba@users.noreply.github.com>
2025-04-02 10:50:53 +0200

    Small Update

--- littlelemon/src/App.test.js
+++ littlelemon/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import ReservationForm from './Commponents/ReservationForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders a ReservationForm heading', () => {
+  render(<ReservationForm />);
+  const headingElement = screen.getByText('Reserve a table');
+  expect(headingElement).toBeInTheDocument();
 });
+
+test('')
