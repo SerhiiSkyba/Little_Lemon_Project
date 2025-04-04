62422737edd7d1a4c1cdd62f442d4610de9561de
Blackhole <128896418+SerhiiSkyba@users.noreply.github.com>
2025-04-04 11:46:19 +0200

    Trying to make test

--- littlelemon/src/App.test.js
+++ littlelemon/src/App.test.js
@@ -1,12 +1,23 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 import "@testing-library/jest-dom";
-import ReservationForm from './Commponents/ReservationForm';
+import ReservationForm from './Components/ReservationForm';
 import ReservationSection from './Components/ReservationSection';
 
 test('Renders a ReservationForm heading', () => {
-  render(<ReservationForm />);
+  render(<ReservationSection><ReservationForm /></ReservationSection>);
   const headingElement = screen.getByText('Reserve a Table');
   expect(headingElement).toBeInTheDocument();
+});
+
+const mockForm = jest.fn();
+jest.mock("./ReservationSection", () => (props) => {
+  mockForm(props);
+  return <mock-ReservationSection />;
+});
+
+test('Test if the fetchAPI working', () =>{
+  const reservation = render(<ReservationSection><ReservationForm state={state} dispatch={dispatch}/></ReservationSection>);
+  expect()
 });
 
